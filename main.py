d8aa033fd2a148a60d8fd8cadbfc7511c5bbeb4c
Fabio Masciarelli <158151713+FabioMasciarelli@users.noreply.github.com>
2025-03-22 14:29:15 +0100

    feat: scaffolding code

--- main.py
+++ main.py
@@ -3,5 +3,18 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<h1>Hello, World!</h1>"
+def main():
+    from yt_dlp import YoutubeDL
+
+    link = "" 
+    output_folder = "./videos" 
+
+    ydl_opts = {
+        'format': 'bestvideo+bestaudio/best', 
+        'merge_output_format': 'mp4', 
+        'outtmpl': f'{output_folder}/%(title)s.%(ext)s',
+    }
+
+    with YoutubeDL(ydl_opts) as ydl:
+        ydl.download([link])
+        return "<h1>Hello, World!</h1>"
